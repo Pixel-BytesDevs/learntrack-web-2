0dd110577756817ebf1ef1718cf0ff89669511a8
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2026-05-06 07:04:48 -0500

    fix urls

--- src/environments/services/service.dev.ts
+++ src/environments/services/service.dev.ts
@@ -1,17 +1,17 @@
 export const Services = Object.freeze({
     gestorGrafo: {
-        url: 'http://26.138.194.69:8083',
+        url: 'http://3.145.22.173:8083',
     },
     moduloAlumno: {
-        cuestionarioUrl: 'http://26.138.194.69:8092/preguntas',
-        insertCuestionarioIrl: 'http://26.138.194.69:8092/cuestionarios/respuestas?reemplazar=true',
-        cuestionarioNivel: 'http://26.138.194.69:8092/api/placement',
-        grafoEstudiante: 'http://26.138.194.69:8092/api/usuario-topic',
-        evaluacion: 'http://26.138.194.69:8092/api/recommendation-evaluation',
-        alumno: 'http://26.138.194.69:8092/alumno',
-        recomendation: 'http://26.138.194.69:5000/api/v1/recommendations',
+        cuestionarioUrl: 'http://3.145.22.173:8092/preguntas',
+        insertCuestionarioIrl: 'http://3.145.22.173:8092/cuestionarios/respuestas?reemplazar=true',
+        cuestionarioNivel: 'http://3.145.22.173:8092/api/placement',
+        grafoEstudiante: 'http://3.145.22.173:8092/api/usuario-topic',
+        evaluacion: 'http://3.145.22.173:8092/api/recommendation-evaluation',
+        alumno: 'http://3.145.22.173:8092/alumno',
+        recomendation: 'http://3.145.22.173:5000/api/v1/recommendations',
     },
     auth: {
-        login: 'http://26.138.194.69:1000/auth',
+        login: 'http://3.145.22.173:1000/auth',
     }
 })
